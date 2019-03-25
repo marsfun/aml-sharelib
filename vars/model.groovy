a100aeb609af25360c47381fdc873a0f66911573
yuanfang <yuanfang@alauda.io>
2019-03-25 23:48:27 +0800

    add deploy

--- vars/model.groovy
+++ vars/model.groovy
@@ -56,11 +56,6 @@ def buildImage(Map runtime,Map source,Map image,Map build){
             env.SVN_REVISION = SVN_REVISION
             env.CODE_COMMIT = SVN_REVISION
         }
-        echo "seesee model folder2"
-        // sh '''
-        //     ls -l ${source.relative_directory}
-        // '''
-
     }
     dir(source.relative_directory) {
         retry(build.retry_count) {
@@ -84,6 +79,53 @@ def buildImage(Map runtime,Map source,Map image,Map build){
             }
         }
     }
+}
 
+def deployModelService(Map modelservice){
+    alaudaDevops.withCluster() {
+        // 指定namespace
+        alaudaDevops.withProject() {
+            def kind = modelservice.meta_kind
+            def instance_name = modelservice.meta_instance_name
+            def start_state = modelservice.start_state
+            def _deploy_timeout_ = modelservice.deploy_timeout
+            def _deploy_timeout_unit_ = modelservice.deploy_timeout_unit
 
+            def isFound = alaudaDevops.selector(kind, instance_name).exists()
+            echo "检查 ${kind}/${instance_name} 实例是否存在? ${isFound}"
+            
+            if (!isFound){
+                error "internal error: ${kind}/${instance_name} not found"
+            }
+            //
+            wantedobject =  alaudaDevops.selector(kind,instance_name).object()
+            wantedobject.status.state = start_state
+            alaudaDevops.apply(wantedobject)
+            //watch
+            def pod = alaudaDevops.selector(kind,instance_name)
+            echo pod.object().status.state
+            timeout(time: _deploy_timeout_, unit: _deploy_timeout_unit_){
+                pod.watch {
+                    echo "开始watch 资源 ${kind}/${instance_name}"
+                    if ( it.count() == 0 ) {
+                        echo "goto next watch loop [cause: ${instance_name} not found."
+                        return false
+                    }
+                    def allDone = true
+                    def current_state =''
+                    it.withEach {
+                        current_state = it.object().status.state
+                        if ( current_state.equals(start_state) ) {
+                            echo "state=${current_state}, 没有达到预期状态 [!${start_state}], go to next watch loop"
+                            allDone = false
+                        }
+                    }
+                    if (allDone){
+                        echo "current_state=${current_state}, OK 达到预期状态 [!${start_state}], exit watch loop"
+                    }
+                    return allDone
+                }
+            }
+        }
+    }
 }
