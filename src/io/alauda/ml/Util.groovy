943f84f8430768c4b81049f3f0f17f80667a3c4a
yuanfang <yuanfang@alauda.io>
2019-04-04 13:58:07 +0800

    change getModelVersionContextPath impl

--- src/io/alauda/ml/Util.groovy
+++ src/io/alauda/ml/Util.groovy
@@ -1,25 +1,6 @@
 package io.alauda.ml
 
 def getModelVersionContextPath(orig, version){
-    /*
-    def spliter = "/"
-    def arr = orig.split(spliter)
-
-    if (arr.length<1){
-        return orig
-    }
-    arr[arr.length-1] = version
-    // return "".join("/",arr)
-    def result=""
-    arr.eachWithIndex { vv,index->
-        result+=vv
-        if (index!=arr.size()-1){
-            result+=spliter
-        }
-    }
-    return result
-    */
-
     def pos = orig.lastIndexOf("/")
     if(pos==-1){
         return orig
@@ -34,4 +15,3 @@ def getModelNameAndContext(relativePath){
     }
     return [relativePath.substring(pos+1),relativePath.substring(0,pos)]
 }
-println getModelVersionContextPath("docker/half_plus_three/00000123","13")
