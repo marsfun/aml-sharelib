8d7ecd4d1e88c462cb47b958f87a871b0ea97bfc
yuanfang <yuanfang@alauda.io>
2019-04-23 16:43:42 +0800

    add pvc logic

--- vars/model.groovy
+++ vars/model.groovy
@@ -10,6 +10,7 @@ def buildImage(Map runtime,Map source,Map image,Map build){
 
     try{
         timeout(time: runtime.timeout_value, unit: runtime.timeout_unit){
+            //always check crd if exists firstly
             alaudaDevops.withCluster() {
                 alaudaDevops.verbose()
             // 指定namespace
@@ -27,62 +28,63 @@ def buildImage(Map runtime,Map source,Map image,Map build){
                     alaudaDevops.apply(wantedobject)
                 }
             }
+            if (source.isModelInPvc !="true"){
+                if (source.repo_type != "SVN"){
+                    env.BRANCH = source.branch
+                    def scmVars = checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: source.branch]],
 
-            if (source.repo_type != "SVN"){
-                env.BRANCH = source.branch
-                def scmVars = checkout([
-                    $class: 'GitSCM',
-                    branches: [[name: source.branch]],
+                        extensions: [[
+                        $class: 'SubmoduleOption',
+                        recursiveSubmodules:
+                        true,
+                        reference: '',
+                        ],[
+                        $class: 'RelativeTargetDirectory',
+                            relativeTargetDir: source.relative_directory
+                        ]],
+                        userRemoteConfigs:
+                        [[
+                        credentialsId: source.credential_id,
+                        url: source.code_repo
 
-                    extensions: [[
-                    $class: 'SubmoduleOption',
-                    recursiveSubmodules:
-                    true,
-                    reference: '',
-                    ],[
-                    $class: 'RelativeTargetDirectory',
-                        relativeTargetDir: source.relative_directory
-                    ]],
-                    userRemoteConfigs:
-                    [[
-                    credentialsId: source.credential_id,
-                    url: source.code_repo
-
-                    ]]
-                ])
-                env.GIT_COMMIT = scmVars.GIT_COMMIT
-                env.GIT_BRANCH = scmVars.GIT_LOCAL_BRANCH
+                        ]]
+                    ])
+                    env.GIT_COMMIT = scmVars.GIT_COMMIT
+                    env.GIT_BRANCH = scmVars.GIT_LOCAL_BRANCH
+                } //git
+                if (source.repo_type == "SVN") {
+                    if (RELATIVE_DIRECTORY == ''){
+                        env.RELATIVE_DIRECTORY = "."
+                    }
+                    def scmVars = checkout([
+                        $class: "SubversionSCM",
+                        additionalCredentials:
+                        [],
+                        excludedCommitMessages: "",
+                        excludedRegions: "",
+                        excludedRevprop:
+                        "",
+                        excludedUsers: "",
+                        filterChangelog: false,
+                        ignoreDirPropChanges:
+                        false,
+                        includedRegions: "",
+                        locations: [[
+                            credentialsId:
+                        "${CREDENTIAL_ID}", depthOption: "infinity", ignoreExternalsOption: true,
+                        local: "${RELATIVE_DIRECTORY}", remote: "${CODE_REPO}"
+                        ]],
+                        quietOperation:
+                        true, workspaceUpdater: [$class: "UpdateUpdater"]
+                    ])
+                    SVN_REVISION = scmVars.SVN_REVISION
+                    env.SVN_REVISION = SVN_REVISION
+                    env.CODE_COMMIT = SVN_REVISION
+                } //svn
             }
-            if (source.repo_type == "SVN") {
-                if (RELATIVE_DIRECTORY == ''){
-                    env.RELATIVE_DIRECTORY = "."
-                }
-                def scmVars = checkout([
-                    $class: "SubversionSCM",
-                    additionalCredentials:
-                    [],
-                    excludedCommitMessages: "",
-                    excludedRegions: "",
-                    excludedRevprop:
-                    "",
-                    excludedUsers: "",
-                    filterChangelog: false,
-                    ignoreDirPropChanges:
-                    false,
-                    includedRegions: "",
-                    locations: [[
-                        credentialsId:
-                    "${CREDENTIAL_ID}", depthOption: "infinity", ignoreExternalsOption: true,
-                    local: "${RELATIVE_DIRECTORY}", remote: "${CODE_REPO}"
-                    ]],
-                    quietOperation:
-                    true, workspaceUpdater: [$class: "UpdateUpdater"]
-                ])
-                SVN_REVISION = scmVars.SVN_REVISION
-                env.SVN_REVISION = SVN_REVISION
-                env.CODE_COMMIT = SVN_REVISION
-            }
-           
+            
             def util = new Util()
             (model_name,context)=util.getModelNameAndContext(source.model_path)
             // println "extract model_name= "+model_name
